190acd21f67b4e216cfd31902024c4da96ee4b5a
(MMirang) <alfkd1024@gmail.com>
2024-07-24 14:40:49 +0900

    Feat: 리엑트로 버튼 클릭 시 사용자 수 증가되고 사용자 목록 보여주는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 리엑트 모듈에서 훅(Hook) 가져오기 : use~로 시작, 여기도 구조분해할당임!
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [users, setUsers] = useState([
+    { name: "Neo", age: 51 },
+    { name: "Lewis", age: 22 },
+    { name: "Evan", age: 18 },
+  ]);
 
+  // let count = 31;
+  // 반응형 데이터, 초기값 세팅, 배열 데이터의 구조분해할당
+  // [데이터와 데이터를 갱신하는 함수]
+  const [count, setCount] = useState(31);
+  function increase() {
+    // count += 1;
+    setCount(count + 1);
+    console.log(count);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{count}명</div>
+      <button onClick={increase}>증가+</button>
+      <ul>
+        {users.map((user) => (
+          <li key={user.name}>{user.name}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
