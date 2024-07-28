680e75f6915d0e53a1327283dc8efcb7e0a96d33
(MMirang) <alfkd1024@gmail.com>
2024-07-28 21:06:29 +0900

    Edit: 로딩 애니메이션 추가, 예외처리 추가,

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // 리엑트 모듈에서 훅(Hook) 가져오기 : use~로 시작, 여기도 구조분해할당임!
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import TheLoader from './components/TheLoader'
 
 export interface Root {
   total: number
@@ -27,6 +28,13 @@ export default function App() {
   const [count, setCount] = useState(31) // 초기값이 있는 경우에는 따로 타입 지정 필요 없다
   // 반응형 데이터, 초기값 세팅, 배열 데이터의 구조분해할당
   // [데이터와 데이터를 갱신하는 함수]
+  const [message, setMessage] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  // useEffect(콜백, 의존성 배열)
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   function increase() {
     // count += 1;
@@ -34,23 +42,38 @@ export default function App() {
     // console.log(count);
   }
 
-  // useEffect(콜백, 의존성 배열)
-  useEffect(() => {
-    getUsers()
-  }, [])
 
-  // 호이스팅됨
   async function getUsers() {
-    const res = await fetch('https://api.heropy.dev/v0/users')
-    const data = await res.json()
-    console.log('응답결과', data)
-    setUsers(data.users)
+    try {
+      // 의도적으로 시간 늦추는 방법, 프로미스 객체,
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      // 외부 요인(에러 발상할 가능성이 있는 코드) = 패치 함수쪽
+      const res = await fetch('https://api.heropy.dev/v0/users')
+      const data = await res.json()
+      console.log('이행, 응답결과', data)
+      setUsers(data.users)
+    } catch (error) {
+      // 에러 클래스의 에러 객체, instanceof는 JS 문법, 타입 가드 필요???
+
+      if (error instanceof Error) {
+        const message = '폭발!'
+        console.error('에러 발생', message)
+        setMessage(message)
+      }
+    } finally {
+      // false로 바꾸는 이유: 
+      setLoading(false)
+    }
   }
+
 
   return (
     <>
       <div>{count}명</div>
       <button onClick={increase}>증가+</button>
+      {/* <div>{loading ? '로딩 중...' : ''}</div> */}
+      <div>{loading && <TheLoader />}</div>
+      <div>{message}</div>
       <ul>
         {users.map(user => (
           <li key={user.name}>{user.name}</li>
---
+++ src/components/TheLoader.jsx
@@ -0,0 +1,5 @@
+import styles from './TheLoader.module.css'
+
+export default function TheLoader() {
+  return <div className={styles.theLoader}></div>
+}
